985a836fe5cb057abbd69b5d0ebf16738e0848c2
Courtney Wilson <cmwilson132@gmail.com>
2021-09-03 22:18:23 -0500

    added basic controllers with no methods

---
+++ app/controllers/shows_controller.rb
@@ -0,0 +1,24 @@
+class ShowsController < ApplicationController
+
+  get "/shows" do
+    <h1>Shows Page</h1>
+  end
+
+  get "/shows/:id" do
+    <h1>Individual Show Page</h1>
+  end
+
+  # post "/shows" do
+
+  # end
+
+  # patch "/shows/:id" do
+
+  # end
+
+  # delete "/shows/:id" do
+
+  # end
+
+
+end
