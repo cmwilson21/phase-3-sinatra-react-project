263b3efdb73966bd854884b5ece78991b2bb03e0
Courtney Wilson <cmwilson132@gmail.com>
2021-09-04 21:43:27 -0500

    Cleaned up controllers

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,5 +1,57 @@
 class CharactersController < ApplicationController
 
+  get "/characters" do
+    Character.all.to_json(include: [:show])
+  end
 
+  get "/characters/:id" do
+    find_character
+    character_to_json
+  end
+
+  post "/characters" do
+    # binding.pry
+    @character = Character.new(params)
+    if @character.save
+      # return object as json if saved
+      character_to_json
+    else
+      # return error message if not saved
+      character_errors_messages
+    end
+  end
+
+  patch "/characters/:id" do
+    find_character
+    if @character.update(params)
+      character_to_json
+    else
+      character_errors_messages
+    end
+  end
+
+  delete "/characters/:id" do
+    find_character
+    if @character
+      @character.destroy
+      @character.to_json
+    else
+      { errors: ["Character Doesn't Exist"] }.to_json
+    end
+  end
+
+  private
+
+  def find_character
+    @character = Character.find_by_id(params["id"])
+  end
+
+  def character_to_json
+    @character.to_json(include: [:show])
+  end
+
+  def character_errors_messages
+    { errors: @character.errors.full_messages }.to_json
+  end
 
 end
--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -7,41 +7,52 @@ class ShowsController < ApplicationController
   end
 
   get "/shows/:id" do
-    show = Show.find_by_id(params["id"])
-    show.to_json(include: [:characters])
+    find_shows
+    shows_to_json
   end
 
   post "/shows" do
     # binding.pry
-    show = Show.new(params)
-    if show.save
+    @show = Show.new(params)
+    if @show.save
       # return object as json if saved
-      show.to_json(include: [:characters])
+      shows_to_json
     else
       # return error message if not saved
-      { errors: show.errors.full_messages }.to_json
+      shows_errors_messages
     end
   end
 
   patch "/shows/:id" do
-    show = Show.find_by_id(params["id"])
-    binding.pry
+    find_shows
     if show.update(params)
-      show.to_json(include: [:characters])
+      shows_to_json
     else
-      { errors: show.errors.full_messages }.to_json
+     shows_errors_messages
     end
   end
 
   delete "/shows/:id" do
-    show = Show.find_by_id(params["id"])
-    if show
-      show.destroy
-      show.to_json
+    find_shows
+    if @show
+      @show.destroy
+      @show.to_json
     else
       { errors: ["Show Doesn't Exist"] }.to_json
     end
   end
 
+  private
+  def find_shows
+    @show = Show.find_by_id(params["id"])
+  end
+
+  def shows_to_json
+    @show.to_json(include: [:characters])
+  end
+
+  def shows_errors_messages
+    { errors: @show.errors.full_messages }.to_json
+  end
 
 end
